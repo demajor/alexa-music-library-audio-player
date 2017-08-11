9b3b84d243936409af74866003a4b0d5351e480c
Doug Major <demajor@demajorMBP.local>
2017-08-10 17:21:46 -0700

    initial commit

---
+++ js/audioAssets.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Audio Files Source - Amazon S3 : https://s3.amazonaws.com/dougmajormusicfiles/
+var audioData = [
+    {
+      'title' : 'Into The Darkness',
+      'url' : 'https://s3.amazonaws.com/dougmajormusicfiles/1_Into_The_Darkness.mp3'
+    },
+    {
+      'title' : 'New Mexico Sunrise',
+      'url' : 'https://s3.amazonaws.com/dougmajormusicfiles/2_New_Mexico_Sunrise.mp3'
+    }
+];
+
+module.exports = audioData;
